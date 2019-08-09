24a78798f3030c9facbe0e9a3b939172b509efe2
Fileto Paixão <filetopaixaowd@gmail.com>
2019-08-09 10:11:50 -0300

    Criação dos models e registro no admin

---
+++ core/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Marca, Veiculos, Pessoa, Parametros, MovRotativo, Mensalista, MovMensalista
+
+
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ('checkin', 'checkout', 'valor_hora', 'veiculo', 'pago', 'total', 'horas_total')
+
+# class VeiculoApp(admin.ModelAdmin):
+#     list_display = ('nome', 'placa', 'proprietario')
+#     search_fields = ("proprietario__nome",)
+
+
+
+admin.site.register(Marca)
+admin.site.register(Veiculos)
+admin.site.register(Pessoa)
+admin.site.register(Parametros)
+admin.site.register(Mensalista)
+admin.site.register(MovMensalista)
+admin.site.register(MovRotativo, MovRotativoAdmin)
