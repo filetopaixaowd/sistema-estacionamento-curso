4b5a9326ae40b174377c10e22d21482aa26d0437
Fileto Paixão <filetopaixaowd@gmail.com>
2019-08-09 13:06:27 -0300

    Implementação do Read dos Models

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, lista_pessoas, lista_veiculos, lista_mov_rot, lista_mensalista, lista_mov_mes
 
 urlpatterns = [
     path('', home),
+    path('pessoas/', lista_pessoas),
+    path('veiculos/', lista_veiculos),
+    path('movimento-rotativo/', lista_mov_rot),
+    path('mensalistas/', lista_mensalista),
+    path('movimento-mensalista/', lista_mov_mes),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pessoa, Veiculos, MovRotativo, Mensalista, MovMensalista
 
 def home(request):
-    return HttpResponse('Core - Home')
+    context = {'mensagem': 'Core - Home'}
+    return render(request, 'core/index.html', context)
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'core/lista_pessoas.html', {'pessoas':  pessoas})
+
+def lista_veiculos(request):
+    veiculos = Veiculos.objects.all()
+    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+def lista_mov_rot(request):
+    mov_rot = MovRotativo.objects.all()
+    return render(request,'core/lista_mov_rot.html', {'mov_rot': mov_rot})
+
+def lista_mensalista(request):
+    mensalistas = Mensalista.objects.all()
+    return render(request, 'core/lista_mensalistas.html', {'mensalistas': mensalistas})
+
+def lista_mov_mes(request):
+    mov_mensal = MovMensalista.objects.all()
+    return render(request, 'core/lista_mov_mensal.html', {'mov_mensal': mov_mensal})
+
