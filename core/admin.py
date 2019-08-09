77fa7c20d6ab589359a339a7ea4149f8a396740b
Fileto Paixão <filetopaixaowd@gmail.com>
2019-08-09 10:28:51 -0300

    Configuração das URLS da APP Core

--- core/admin.py
+++ core/admin.py
@@ -6,6 +6,9 @@ from .models import Marca, Veiculos, Pessoa, Parametros, MovRotativo, Mensalista
 class MovRotativoAdmin(admin.ModelAdmin):
     list_display = ('checkin', 'checkout', 'valor_hora', 'veiculo', 'pago', 'total', 'horas_total')
 
+class MovMensalistaAdmin(admin.ModelAdmin):
+    list_display = ('mensalista', 'dt_pago', 'total')
+
 # class VeiculoApp(admin.ModelAdmin):
 #     list_display = ('nome', 'placa', 'proprietario')
 #     search_fields = ("proprietario__nome",)
@@ -17,5 +20,5 @@ admin.site.register(Veiculos)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
 admin.site.register(Mensalista)
-admin.site.register(MovMensalista)
+admin.site.register(MovMensalista, MovMensalistaAdmin)
 admin.site.register(MovRotativo, MovRotativoAdmin)
